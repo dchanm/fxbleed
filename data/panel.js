cd92197608898cee178fc6342c602c08952346d5
Garrett Robinson <garrettr@riseup.net>
2014-04-10 15:55:40 -0700

    remove accidental indentation

--- data/panel.js
+++ data/panel.js
@@ -1,7 +1,7 @@
-  document.querySelector('input#autoClearCookies').addEventListener('change', function() {
-    self.port.emit("changeSettings", { name: "autoClearCookies", value: this.checked });
-  });
+document.querySelector('input#autoClearCookies').addEventListener('change', function() {
+  self.port.emit("changeSettings", { name: "autoClearCookies", value: this.checked });
+});
 
-  document.querySelector('input#addonEnabled').addEventListener('change', function() {
-    self.port.emit("changeSettings", { name: "addonEnabled", value: this.checked });
-  });
+document.querySelector('input#addonEnabled').addEventListener('change', function() {
+  self.port.emit("changeSettings", { name: "addonEnabled", value: this.checked });
+});
