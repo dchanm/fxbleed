97211f1344693ab4cd764a51269b82171db67177
Garrett Robinson <garrettr@riseup.net>
2014-04-11 10:59:09 -0700

    update panel with current site status

--- data/panel.js
+++ data/panel.js
@@ -5,3 +5,17 @@ document.querySelector('input#autoClearCookies').addEventListener('change', func
 document.querySelector('input#addonEnabled').addEventListener('change', function() {
   self.port.emit("changeSettings", { name: "addonEnabled", value: this.checked });
 });
+
+self.port.on("siteStatus", function(status) {
+  var statusSpan = document.querySelector('div#header span#status');
+  if (status.vulnerable === true) {
+    statusSpan.textContent = "vulnerable";
+  } else if (status.vulnerable === false) {
+    statusSpan.textContent = "not vulnerable";
+  } else {
+    statusSpan.textContent = "unknown";
+  }
+
+  var hostSpan = document.querySelector('div#header span#host');
+  hostSpan.textContent = status.host;
+});
