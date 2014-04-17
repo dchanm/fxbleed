b586bd1112a18d051082a84bd326b8e7be01eae4
Garrett Robinson <garrettr@riseup.net>
2014-04-17 11:23:39 -0700

    wire up "scan my history" button on first run page

--- data/historyCheck.js
+++ data/historyCheck.js
@@ -1,4 +1,4 @@
-console.log("ran history-check.js");
+console.log("ran historyCheck.js");
 
 self.port.on("historyCheckSiteUpdate", function(update) {
   console.log("contents cript got historyCheckSiteUpdate, update=", update);
