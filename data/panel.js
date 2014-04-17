4d963fc4a27adb12d50e17d2189b5ba29e7b0877
Garrett Robinson <garrettr@riseup.net>
2014-04-17 08:40:18 -0700

    disable half-baked cookie clearing and hide pref

--- data/panel.js
+++ data/panel.js
@@ -1,6 +1,8 @@
+/*
 document.querySelector('input#autoClearCookies').addEventListener('change', function() {
   self.port.emit("changeSettings", { name: "autoClearCookies", value: this.checked });
 });
+*/
 
 document.querySelector('input#addonEnabled').addEventListener('change', function() {
   self.port.emit("changeSettings", { name: "addonEnabled", value: this.checked });
