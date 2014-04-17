c2ce9e43f50b0dd0e2dad71c75e1e82ff85a6daa
Garrett Robinson <garrettr@riseup.net>
2014-04-17 16:37:28 -0700

    rough but working history scan (!!!)

--- data/historyCheck.js
+++ data/historyCheck.js
@@ -1,5 +1,25 @@
 console.log("ran historyCheck.js");
 
-self.port.on("historyCheckSiteUpdate", function(update) {
-  console.log("contents cript got historyCheckSiteUpdate, update=", update);
+self.port.on("checkResult", function(result) {
+  console.log("got result for " + result.domain + ": " + result.code);
+  var resultLi = document.createElement("li");
+
+  // TODO: Do I need to sanitize this input (since I'm inserting it into the
+  // document)? Potential attack: try to go to <script>doBadStuff()</script>,
+  // then open the history scan.
+  resultLi.innerHTML = '<a href="' + result.domain + '">' + result.domain + '</a>';
+  switch(result.code) {
+    case 0:
+      resultLi.className = 'vulnerable';
+      break;
+    case 1:
+      resultLi.className = 'notVulnerable';
+      break;
+    case 2:
+      resultLi.className = 'error';
+      break;
+  }
+
+  var resultsList = document.querySelector('ol#results');
+  resultsList.appendChild(resultLi);
 });
