24454347cd1d394e4ad8a7c9e558b88ddf264719
Garrett Robinson <garrettr@riseup.net>
2014-04-17 18:56:23 -0700

    add ui for sites that are not vulnerable, but once were

--- data/historyCheck.js
+++ data/historyCheck.js
@@ -5,16 +5,25 @@ self.port.on("checkResult", function(result) {
   // TODO: Do I need to sanitize this input (since I'm inserting it into the
   // document)? Potential attack: try to go to <script>doBadStuff()</script>,
   // then open the history scan.
+  // TODO: basic templating, should build these from an object
   resultLi.innerHTML = '<a href="' + result.domain + '">' + result.domain + '</a>';
   switch(result.code) {
     case 0:
       resultLi.className = 'vulnerable';
+      resultLi.innerHTML += '<span class="explanation">Vulnerable!</span>';
       break;
     case 1:
-      resultLi.className = 'notVulnerable';
+      if (result.wasVulnerable) {
+        resultLi.className = 'notVulnerable wasVulnerable';
+        resultLi.innerHTML += '<span class="explanation">This site was vulnerable, but is now fixed.<br>Consider changing your password!</span>';
+      } else {
+        resultLi.className = 'notVulnerable';
+        resultLi.innerHTML += '<span class="explanation">Not vulnerable</span>';
+      }
       break;
     case 2:
       resultLi.className = 'error';
+      resultLi.innerHTML += '<span class="explanation">An error occurred checking this site (<a href="https://filippo.io/Heartbleed/faq.html#wentwrong">learn more</a>)</span>';
       break;
   }
 
